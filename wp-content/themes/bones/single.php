e3c2fd0ae01b9dd437139aa939f400b1b42e6a36
Corey Margulis <coreymargulis@gmail.com>
2014-10-08 15:49:54 -0500

    fixed header images and about text size on home page

--- wp-content/themes/bones/single.php
+++ wp-content/themes/bones/single.php
@@ -2,13 +2,13 @@
 
 <?php $bg = get_field( 'hero-image' ); ?>
 
-<div class="hero-image" style="background-image: url(<?=$bg?>)"></div>
 
-<!--
+
+
 <div class="hero-image-container">
-	
+	<div class="hero-image" style="background-image: url(<?=$bg?>)"></div>
 </div>
--->
+
 
 
 			<div id="content">
