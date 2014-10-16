04d09a25832cc838af5059b04cb0ba4dd19c1967
Corey (main) <coreymargulis@gmail.com>
2014-10-16 00:38:44 -0500

    1.0

--- wp-content/themes/bones/page-projects.php
+++ wp-content/themes/bones/page-projects.php
@@ -14,7 +14,7 @@
 			<li><a id="commencement" href="commencement">Commencement</a></li>
 			<li><a id="pastel-artist" href="pastel-artist">Pastel Artist</a></li>
 			<li><a id="sullivan-galleries" href="sullivan-galleries">Sullivan Galleries</a></li>
-			<li><a id="ongoing" href="ongoing">Campus Signs</a></li>
+			<!-- <li><a id="ongoing" href="ongoing">Campus Signs</a></li> -->
 			<!-- <li><a id="about" href="/about">About</a> -->
 			<!-- <li><a href="/blog">Blog</a></li> -->
 		</ul>
@@ -65,7 +65,9 @@
 
 </div>
 
+<!--
 <div class="profile">
 	<a id="profile-link" href="/">About</a>
 </div>
+-->
 
